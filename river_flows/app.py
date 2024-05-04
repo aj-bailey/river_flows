a9646780b75bc87a045271dbaa936d75bb354df4
Adam Bailey <ab67319@gmail.com>
2024-05-04 08:58:42 -0600

    Update app to initialize database on startup

--- river_flows/app.py
+++ river_flows/app.py
@@ -2,8 +2,11 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from river_flows.utils.db import initialize_db
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    initialize_db()
     yield
 
 
