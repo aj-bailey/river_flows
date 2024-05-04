0adbb66e6494d524b99a6b96bdeae1d6e589c6eb
Adam Bailey <ab67319@gmail.com>
2024-05-04 08:58:26 -0600

    Add database initialization module

---
+++ river_flows/utils/db.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy_utils import create_database, database_exists
+
+from river_flows.config.config import DATABASE_URL
+from river_flows.data.exceptions import DatabaseInitializationException, DatabaseEngineException
+
+
+_engine: Engine = create_engine(url=DATABASE_URL, pool_pre_ping=True)
+TransactionalSession = sessionmaker(_engine)
+
+def initialize_db():
+    try:
+        print("Initializing database...")
+
+        if database_exists(DATABASE_URL):
+            print("Database exists!")
+        else:
+            create_database(DATABASE_URL)
+            print("Database created!")
+    except Exception:
+        raise DatabaseInitializationException
+
+@contextmanager
+def get_session() -> Session:
+    session = TransactionalSession()
+
+    try:
+        yield session
+    except Exception as e:
+        raise DatabaseEngineException(str(e))
+    finally:
+        session.close()
