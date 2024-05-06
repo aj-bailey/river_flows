f8b002c28327c31bfaa3f1c005f459cae735fbf7
Adam Bailey <ab67319@gmail.com>
2024-05-05 18:04:02 -0600

    Feat: Add site condition orm and data model

--- river_flows/data/site_condition.py
+++ river_flows/data/site_condition.py
@@ -1,11 +1,16 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 
 
 class SiteCondition(BaseModel):
+    id: Optional[int] = None
     site_id: str
     site_name: str
     timestamp: datetime
     value: int
     unit: str
+
+    class Config:
+        from_attributes = True
---
+++ river_flows/orm/site_condition.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from river_flows.orm.base import Base
+from river_flows.orm.mixins import TimestampMixin
+
+class SiteCondition(Base, TimestampMixin):
+    __tablename__ = "site_conditions"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    site_id: Mapped[str] = mapped_column(String)
+    site_name: Mapped[str] = mapped_column(String)
+    timestamp: Mapped[datetime] = mapped_column(DateTime)
+    value: Mapped[int] = mapped_column(Integer)
+    unit: Mapped[str] = mapped_column(String)
