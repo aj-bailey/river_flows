1028b4ac35238423afb01a7336fff2743721dac8
Adam Bailey <ab67319@gmail.com>
2024-05-11 08:28:02 -0600

    Test: Add test__timeframe_river_flow__success

--- tests/unit/clients/usgs_client_test.py
+++ tests/unit/clients/usgs_client_test.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from dateutil.parser import parse
 
 import vcr
@@ -6,7 +5,9 @@ import vcr
 from river_flows.clients.usgs_client import USGSClient
 from river_flows.data.site_condition import SiteCondition
 
-@vcr.use_cassette('tests/fixtures/unit/clients/usgs_client/test__current_flow__succcess.yaml')
+TEST_PATH = 'tests/fixtures/unit/clients/usgs_client/'
+
+@vcr.use_cassette(TEST_PATH + 'test__current_flow__succcess.yaml')
 def test__current_flow__success():
     # Arrange
     usgs_client = USGSClient()
@@ -24,3 +25,19 @@ def test__current_flow__success():
     )
 
     assert site_condition == expected_site_condition
+
+@vcr.use_cassette(TEST_PATH + 'test__timeframe_river_flow__success.yaml')
+def test__timeframe_river_flow__success():
+    # Arrange
+    usgs_client = USGSClient()
+    start_date = parse("2024-05-01T00:00")
+    end_date = parse("2024-05-02T00:00")
+
+
+    # Act
+    site_conditions = usgs_client.timeframe_river_flow(start_date=start_date, end_date=end_date)
+
+    # Assert
+    assert isinstance(site_conditions, list)
+    assert len(site_conditions) == 97
+    assert all(isinstance(site_condition, SiteCondition) for site_condition in site_conditions)
