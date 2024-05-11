4985e6d426221500fbe670b43125f9fcf3531bce
Adam Bailey <ab67319@gmail.com>
2024-05-11 08:28:32 -0600

    Feat: Add timeframe_river_flow client feature

--- river_flows/clients/usgs_client.py
+++ river_flows/clients/usgs_client.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from dateutil.parser import parse
 import requests
 
 from river_flows.config.config import USGS_EWRSD_SITE, USGS_URL
@@ -12,12 +15,22 @@ class USGSClient():
         response.raise_for_status()
         response_json = response.json()
 
-        site_condition = self._parse_response(response_json)
+        site_condition = self._parse_current_response(response_json)
 
         return site_condition
 
+    def timeframe_river_flow(self, start_date: datetime, end_date: datetime) -> list[SiteCondition]:
+        usgs_uri = f"&startDT={start_date.isoformat()}&endDT={end_date.isoformat()}&siteStatus=all"
+
+        response = requests.get(self.usgs_url + usgs_uri)
+        response.raise_for_status()
+        response_json = response.json()
+
+        site_conditions = self._parse_timeframe_response(response_json)
+
+        return site_conditions
     
-    def _parse_response(self, site_data: dict) -> SiteCondition:
+    def _parse_current_response(self, site_data: dict) -> SiteCondition:
         site_dict = {}
 
         site_dict['site_id'] = site_data["value"]["timeSeries"][1]['sourceInfo']['siteCode'][0]['value']
@@ -27,3 +40,20 @@ class USGSClient():
         site_dict['unit'] = site_data["value"]["timeSeries"][1]['variable']['unit']['unitCode']
 
         return SiteCondition(**site_dict)
+    
+    def _parse_timeframe_response(self, site_condition_json) -> list[SiteCondition]:
+        site_dict = {}
+        site_condition_values = []
+
+        site_dict['site_id'] = site_condition_json["value"]["timeSeries"][1]['sourceInfo']['siteCode'][0]['value']
+        site_dict['site_name'] = site_condition_json["value"]["timeSeries"][1]['sourceInfo']['siteName']
+        site_dict['unit'] = site_condition_json["value"]["timeSeries"][1]['variable']['unit']['unitCode']
+        values = site_condition_json["value"]["timeSeries"][1]['values'][0]['value']
+
+        for value in values:
+            site_dict['timestamp'] = parse(value['dateTime'])
+            site_dict['value'] = value['value']
+
+            site_condition_values.append(SiteCondition(**site_dict))
+        
+        return site_condition_values
