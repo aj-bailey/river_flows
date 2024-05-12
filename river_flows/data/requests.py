e42f852dd803c028cf704f6f3b497a6a3d8d4a99
Adam Bailey <ab67319@gmail.com>
2024-05-12 10:50:28 -0600

    Feat: Add populate site conditions request model

---
+++ river_flows/data/requests.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class PopulateSiteConditionsRequest(BaseModel):
+    start_date: str
+    end_date: str
+    site_id: Optional[str] = None
