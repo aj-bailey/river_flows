7f5613ef15e689f67b1a367df687aae63f617fae
Adam Bailey <ab67319@gmail.com>
2024-05-12 10:50:01 -0600

    Feat: Add populate site conditions endpoint

--- river_flows/app.py
+++ river_flows/app.py
@@ -1,12 +1,18 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
-from river_flows.utils.db import initialize_db
+from river_flows.background_jobs.scheduler import schedule_jobs
+from river_flows.clients.usgs_client import USGSClient
+from river_flows.data.requests import PopulateSiteConditionsRequest
+from river_flows.handlers.populate_site_conditions_handler import PopulateSiteConditionsHandler
+from river_flows.repositories.site_condition_repository import SiteConditionRepository
+from river_flows.utils.db import initialize_db, get_session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     initialize_db()
+    schedule_jobs()
     yield
 
 
@@ -18,3 +24,24 @@ def health():
     return {
         'result': 'GOOD'
     }
+
+
+@app.post("/populate_site_conditions")
+def populate_site_conditions(
+        request_params: PopulateSiteConditionsRequest,
+        session = Depends(get_session),
+        ):
+    site_id = request_params.site_id
+    start_date = request_params.start_date
+    end_date = request_params.end_date
+
+    usgs_client = USGSClient(site=site_id)
+    site_condition_repo = SiteConditionRepository(session)
+    handler = PopulateSiteConditionsHandler(usgs_client=usgs_client, site_condition_repo=site_condition_repo)
+
+    try:
+        count_site_conditions_upserted = handler.handle(start_date=start_date, end_date=end_date)
+    except Exception as e:
+        return {"site_conditions_populated": False}
+
+    return {"site_conditions_populated": True, "count_site_conditions_upserted": count_site_conditions_upserted}
--- tests/integration/app_test.py
+++ tests/integration/app_test.py
@@ -1,3 +1,10 @@
+import vcr
+
+
+TEST_PATH = 'tests/fixtures/integration/app_test/'
+
+
+
 def test__health__success(test_client):
     # Act
     response = test_client.get("/health")
@@ -5,3 +12,17 @@ def test__health__success(test_client):
     
     # Assert
     assert response_json['result'] == "GOOD"
+
+@vcr.use_cassette(TEST_PATH + 'test__populate_site_conditions__success.yaml')
+def test__populate_site_conditions__success(test_client):
+    # Arrange
+    json_data = {"site_id": "09067020", "start_date": "05-11-2005", "end_date": "05-11-2006"}
+
+    # Act
+    response = test_client.post("/populate_site_conditions", json=json_data)
+    response_json = response.json()
+
+    # Assert
+    expected_response_json = {'site_conditions_populated': True, 'count_site_conditions_upserted': 32512}
+
+    assert response_json == expected_response_json
