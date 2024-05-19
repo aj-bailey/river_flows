ac2b52170c7638e87a397765818d8dc5c4434a06
Adam Bailey <ab67319@gmail.com>
2024-05-19 10:39:53 -0600

    Refactor: Update background jobs to grab previous date data

---
+++ river_flows/background_jobs/scheduler.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+from river_flows.background_jobs.save_previous_date_river_flows import save_previous_date_river_flows
+
+
+def schedule_jobs():
+    scheduler = AsyncIOScheduler()
+
+    scheduler.add_job(save_previous_date_river_flows, CronTrigger(hour=8))
+    print("Added save_previous_date_river_flows background job")
+    
+    scheduler.start()
+
