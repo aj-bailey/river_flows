b35b0e09d597d7383c59351846e60d325d504a25
Adam Bailey <ab67319@gmail.com>
2024-09-14 17:14:20 -0600

    Feat: Add get site conditions endpoint

--- river_flows/app.py
+++ river_flows/app.py
@@ -5,8 +5,10 @@ from fastapi import Depends, FastAPI
 
 from river_flows.background_jobs.scheduler import schedule_jobs
 from river_flows.clients.usgs_client import USGSClient
-from river_flows.data.requests import PopulateSiteConditionsRequest
+from river_flows.data.requests import GetSiteConditionsRequest, PopulateSiteConditionsRequest
+from river_flows.data.responses import SiteConditionsResponse
 from river_flows.handlers.populate_site_conditions_handler import PopulateSiteConditionsHandler
+from river_flows.handlers.site_conditions_handler import SiteConditionsHandler
 from river_flows.repositories.site_condition_repository import SiteConditionRepository
 from river_flows.utils.db import initialize_db, get_session
 
@@ -47,9 +49,36 @@ def populate_site_conditions(
 
     return {"site_conditions_populated": True, "count_site_conditions_upserted": count_site_conditions_upserted}
 
+
 @app.get("/date_peak/{date}")
 def date_peak(date: date):
     # TODO 
     return {
         'result': date
     }
+
+
+@app.get("/site_conditions")
+def site_conditions(
+    request_params: GetSiteConditionsRequest = Depends(),
+    session = Depends(get_session),
+):
+    site_id = request_params.site_id
+    start_date = request_params.start_date
+    end_date = request_params.end_date
+
+    site_condition_repo = SiteConditionRepository(session)
+    handler = SiteConditionsHandler(site_condition_repo=site_condition_repo)
+    
+    try:
+        site_conditions = handler.handle(start_date=start_date, end_date=end_date, site_id=site_id)
+    except Exception as e:
+        return SiteConditionsResponse(
+        result="failure",
+        error=str(e)
+    )
+
+    return SiteConditionsResponse(
+        result="success",
+        data=site_conditions
+    )
--- river_flows/handlers/site_conditions_handler.py
+++ river_flows/handlers/site_conditions_handler.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+from typing import Optional
+
 from river_flows.clients.usgs_client import USGSClient
+from river_flows.config.config import USGS_EWRSD_SITE
+from river_flows.data.site_condition import SiteCondition
 from river_flows.repositories.site_condition_repository import SiteConditionRepository
 
 class SiteConditionsHandler():
-    def __init__(self, usgs_client: USGSClient, site_condition_repo: SiteConditionRepository):
-        self.usgs_client = usgs_client
+    def __init__(self, site_condition_repo: SiteConditionRepository):
         self.site_condition_repo = site_condition_repo
+
+    def handle(self, start_date: str, end_date: str, site_id: Optional[str]) -> list[SiteCondition]:
+        start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
+        end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
+
+        if not site_id:
+            site_id = USGS_EWRSD_SITE
+
+        site_conditions = self.site_condition_repo.get_records(start_date=start_datetime, end_date=end_datetime, site_id=site_id)
+        return site_conditions
