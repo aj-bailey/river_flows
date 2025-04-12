94d4b7e504022397f3d15c23582768251039567d
Adam Bailey <ab67319@gmail.com>
2025-04-12 17:04:47 -0600

    Parse client resposne

--- river_flows/clients/snotel_client.py
+++ river_flows/clients/snotel_client.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from functools import reduce
 
+import pandas as pd
 import requests
+
+from river_flows.data.snotel import Snotel
 
 class SnotelAPIClient():
     def __init__(self):
@@ -10,16 +14,11 @@ class SnotelAPIClient():
     def get_station_data(
         self,
         station_triplets: str,
-        begin_date:       str,
-        end_date:         str = None,
-        elements:         str = "*",
-        duration:         str = "HOURLY",
+        begin_date:       datetime,
+        end_date:         datetime,
+        elements:         str = "PREC, SNWD, TOBS, WTEQ",
+        duration:         str = "DAILY",
     ):
-        begin_date = datetime.strptime(begin_date, "%Y-%m-%d")
-
-        if not end_date:
-            end_date = begin_date + relativedelta(years=1) - relativedelta(days=1)
-
         params = {
             "stationTriplets": station_triplets,
             "beginDate": begin_date,
@@ -31,9 +30,29 @@ class SnotelAPIClient():
         response = requests.get(self.base_url + "/data?", params=params)
 
         if response.status_code == 200:
-            return response.json()[0]
+            parsed_response = self._parse_station_data(response.json())
+            return parsed_response
         else:
             raise Exception(
                 f"Failed to fetch data for {begin_date} - {end_date} {station_triplets}: {response.status_code} - {response.text}"
             )
+        
+    def _parse_station_data(self, station_data: dict) -> list[Snotel]:
+        data = station_data[0]
+        station_triplet = data['stationTriplet']
 
+        element_dfs = []
+
+        for element_data in data['data']:
+            element_name = element_data['stationElement']['elementCode']
+            df = pd.DataFrame(element_data['values']).rename(columns={'value': element_name.lower()})
+            element_dfs.append(df)
+
+        dfs_merged = reduce(lambda df1, df2: pd.merge(df1, df2, on='date', how='outer'), element_dfs)
+        dfs_merged = dfs_merged.rename(columns={'date': 'timestamp'})
+        dfs_merged['station_triplets'] = station_triplet
+
+        data_dict = dfs_merged.to_dict(orient='records')
+        snotel_data = [Snotel.model_validate(data) for data in data_dict]
+
+        return snotel_data
