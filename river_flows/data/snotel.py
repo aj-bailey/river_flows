455202f32278a6a4af0fc658bae24b86318546f9
Adam Bailey <ab67319@gmail.com>
2025-04-12 17:05:52 -0600

    Add batch processing snotel data

--- river_flows/data/snotel.py
+++ river_flows/data/snotel.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+
+from river_flows.data.exceptions import SnotelDataException
+
+
+DEFAULT_BATCH_SIZE = 100
 
 class Snotel(BaseModel):
     station_triplets: str
@@ -13,3 +18,27 @@ class Snotel(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class BatchSnotel(BaseModel):
+    batch_size: int
+    batch_snotel: list[list[Snotel]]
+    snotel_data: list[Snotel]
+
+    @model_validator(mode='before')
+    def batch_values(cls, values):
+        if "snotel_data" not in values.keys():
+            raise SnotelDataException()
+
+        if 'batch_size' not in values.keys():
+            values['batch_size'] = DEFAULT_BATCH_SIZE
+
+        values['batch_snotel'] = []
+
+        
+        for i in range(0, len(values['snotel_data']), values['batch_size']):
+            batch = values['snotel_data'][i:i + values['batch_size']]
+            values['batch_snotel'].append(batch)
+
+        return values
+    
