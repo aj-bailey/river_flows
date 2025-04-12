1e92bfde7403bce4e1b09a756cd6edacd556324d
Adam Bailey <ab67319@gmail.com>
2025-04-12 09:28:51 -0600

    Update basemodel attributes

---
+++ river_flows/data/snotel.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+class SiteCondition(BaseModel):
+    station_triplets: str
+    timestamp: datetime
+    prec: Optional[float] = None
+    tobs: Optional[float] = None
+    wteq: Optional[float] = None
+    snwd: Optional[float] = None
+
+    class Config:
+        from_attributes = True
